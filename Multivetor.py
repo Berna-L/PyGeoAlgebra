6455651df6c70c160283440d79e8ce0991f10aa8
Bernardo Lopes Leão <bernardolopes@id.uff.br>
2016-09-20 14:59:57 -0300

    Pequenas alterações

---
+++ Multivetor.py
@@ -0,0 +1,16 @@
+import collections
+
+class Multivetor:
+	multivetor
+
+	def __init__():
+		multivetor = collections.OrderedDict()
+
+	def inserirBase(base: int, escala: long):
+		if (escala != 0):
+			multivetor[base] = escala
+		else:
+			del multivetor[base]
+
+	def removerBase(base: int):
+		inserirBase(base, 0)
